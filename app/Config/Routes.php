f39d44490910c1911893eadf01cfa4224141f470
kauanpscode <kauanps2271@gmail.com>
2026-05-01 23:17:45 -0300

    adequações

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,6 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+// app/Config/Routes.php
+$routes->get('/api/test', 'Home::test');
--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -8,4 +8,11 @@ class Home extends BaseController
     {
         return view('welcome_message');
     }
+
+    public function test()
+    {
+        return $this->response->setJSON([
+            "msg" => "API funcionando"
+        ]);
+    }
 }
